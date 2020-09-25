bca257beace8d05aff4de8af0c3bb7fa5b69de89
samerelhamdo <samer.elhamdoo@gmail.com>
2020-09-25 10:07:56 +0300

    fix DB

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://django-vue-samer.herokuapp.co"'
+  API_URL: '"http://localhost:8000"'
 })
